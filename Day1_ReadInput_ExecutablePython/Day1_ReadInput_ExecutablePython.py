d52353be5624bc9bc4d35fd79a2a5c29719cd38f
sunyumail93 <sunyumail93@gmail.com>
2020-04-07 18:46:59 -0400

    Day1

---
+++ Day1_ReadInput_ExecutablePython/Day1_ReadInput_ExecutablePython.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#This sys package communicate Python with terminal
+import sys
+
+First_argument=sys.argv[1]
+Second_argument=sys.argv[2]
+
+#print the first argument:
+print("First argument:",First_argument)
+print("Second argument:",Second_argument)
---
+++ Day1_ReadInput_ExecutablePython/Day1_readme.txt
@@ -0,0 +1,6 @@
+1, 2020-04-07
+Aim: read input using ‘sys', make the python script executable.
+Code: Day1_ReadInput_ExecutablePython.py
+
+Run using python3 (Make sure python3 installed)
+python3 Day1_ReadInput_ExecutablePython.py a b
